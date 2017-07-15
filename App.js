f78ec11b9c42679a0ab3d519efca097b8fdefd97
victor-iyiola <javafolabi@gmail.com>
2017-07-16 00:53:03 +0100

    Blinking with states & props

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
-import { AppRegistry, Text, View, Image } from 'react-native'
-import Greeting from './src/components/greeting'
+import { AppRegistry, View } from 'react-native'
+import Blink from './src/components/blink-text'
 
 export default class App extends Component {
 
   render() {
-    const pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
     return (
-      <View style={{alignItems: 'center'}}>
-        <Image source={pic} style={{width: 200, height: 200}} />
-        <Text>It works! </Text>
-        <Greeting name='Victor' />
-        <Greeting name="McCain" />
-        <Greeting name="Alabama" />
-        <Greeting name='React Native' />
+      <View style={{ alignItems: 'center' }}>
+        <Blink text='Victor is amazing!' />
+        <Blink text='This guy is lit!' />
+        <Blink text="I'm now on twitter!" />
       </View>
     )
   }
